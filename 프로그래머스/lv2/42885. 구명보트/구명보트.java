5322ba24efd24ec22e6d7672fdc2c1283f86c34c
Hyunwoo Jang <86454635+aswooo@users.noreply.github.com>
2023-08-20 23:58:34 +0900

    [level 2] Title: 구명보트, Time: 8.95 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int index = 0;
+        
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            }
+            else {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

2b178b15d4354b9ca80cfe59b0e2247ff8c86ac0
Hyunwoo Jang <86454635+aswooo@users.noreply.github.com>
2023-10-20 15:47:29 +0900

    [Silver I] Title: 절댓값 힙, Time: 620 ms, Memory: 26860 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,30 +7,27 @@ import java.util.PriorityQueue;
 public class Main{
     public static void main(String[] args) throws IOException {   //프로그램의 시작부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> pos = new PriorityQueue(Comparator.naturalOrder());
-        PriorityQueue<Integer> neg = new PriorityQueue(Comparator.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+                if(Math.abs(o1) == Math.abs(o2))
+                    return o1-o2;
+                else
+                    return Math.abs(o1) - Math.abs(o2);
+            }
+        });
 
         int n = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < n; i++){
             int num = Integer.parseInt(br.readLine());
-            if(num < 0){ // 음수일 경우
-                neg.add(num);
-            }
-            else if(num > 0){   //  양수일 경우
-                pos.add(num);
-            }
-            else {   // 0일 경우
-                if (neg.isEmpty() && pos.isEmpty()) {
+            if(num != 0)
+                pq.add(num);
+            else {
+                if (pq.isEmpty()) {
                     System.out.println("0");
-                } else if (neg.isEmpty()) {
-                    System.out.println(pos.poll());
-                } else if (pos.isEmpty()){
-                    System.out.println(neg.poll());
-                } else if (neg.peek() * -1 <= pos.peek()) {
-                    System.out.println(neg.poll());
                 } else {
-                    System.out.println(pos.poll());
+                    System.out.println(pq.poll());
                 }
             }
         }

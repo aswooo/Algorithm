a6e1ca62ef9f22a0d4766c7d07c7cca6d6494616
Hyunwoo Jang <86454635+aswooo@users.noreply.github.com>
2024-01-02 23:56:21 +0900

    [Silver II] Title: 연속합, Time: 652 ms, Memory: 40312 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;		
+    static Integer[] dp;
+    static int max;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        dp = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        max = arr[0];
+        recursive(N - 1);
+
+        System.out.println(max);
+    }
+
+    static int recursive(int N) {
+        if(dp[N] == null) {
+            dp[N] = Math.max(recursive(N - 1) + arr[N], arr[N]);
+
+            max = Math.max(dp[N], max);
+        }
+
+        return dp[N];
+    }
+}
